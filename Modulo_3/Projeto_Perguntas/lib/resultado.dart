d9a0947500badb76d90cc971598bd8180aac9796
Samuel-sys <alencar5647@gmail.com>
2020-11-06 01:30:46 -0300

    Comentario do o codigo do corpo do resultado do Questionario

--- Modulo_3/Projeto_Perguntas/lib/resultado.dart
+++ Modulo_3/Projeto_Perguntas/lib/resultado.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
+  //final
   final int pontuacao;
   final void Function() function;
 
+  //Declaração da class
+  //os parametros com o termo @required são obrigatorios
   Resultado({@required this.pontuacao, this.function});
 
+  //cria um texto para o usuario conforme a pontuação dele
   String get fraseResultado {
     if (pontuacao < 8) {
       return "Parabens";
@@ -18,28 +22,31 @@ class Resultado extends StatelessWidget {
     }
   }
 
+  //Widget com a mensagem do usuario e um botão para reiniciar a partida
   @override
   Widget build(BuildContext context) {
     return Column(
+      //coloca os dados da coluna no centro da tela
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Center(
           child: Text(
-            fraseResultado,
+            fraseResultado, //metodo que retorna a String de apresentação da UI
             style: TextStyle(
-              fontSize: 28,
+              fontSize: 28, //Tamanho da letra
             ),
           ),
         ),
+        //botão para reiniciar o questionario
         FlatButton(
           child: Text(
             "Reiniciar?",
             style: TextStyle(
-              fontSize: 18,
+              fontSize: 18, //Tamanho da letra
             ),
           ),
-          textColor: Colors.blue,
-          onPressed: this.function,
+          textColor: Colors.blue, //cor do botão
+          onPressed: this.function, //função entrege no parametro da class
         ),
       ],
     );
