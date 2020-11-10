d0f254c2e84b6c4ec46de7437720061f6cac4775
Samuel-sys <alencar5647@gmail.com>
2020-11-10 12:37:59 -0300

    Adicionado novos Objetos a List _transaction da class TransactionUser
    Alteração do SingleChildScrollView para uma ListView.builder
    Criando assim um sistema mais fluido que gasta menos memoria do aparelho fazendo com que se carregue apenas os itens da lista que estão sendo visto

--- Modulo_4/expenses/lib/components/transaction_list.dart
+++ Modulo_4/expenses/lib/components/transaction_list.dart
@@ -11,69 +11,69 @@ class TransactionList extends StatelessWidget {
     return //Columns de transferencias realizadas
         Container(
       height: 300, //defino o tamanho do container da Lista de Tranferencias
-      child: SingleChildScrollView(
-        child: Column(
-          children:
-              //List responsável por apresentar todos os objetos de tranferencia
-              transactions.map((tr) {
-            //manipulando os dado do Objeto Transaction
-            return Card(
-                child: Row(
-              children: [
-                //apresenta o valor da transação
-                Container(
-                  //Adicionando margin ao container que irá apresentar
-                  //o valor das transferências realizadas
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
-                  ),
+      child: ListView.builder(
+        //apresenta a quantidade de itens dentro da List
+        itemCount: transactions.length,
 
-                  //decorando a Caixa que apresenta o preço do produto
-                  decoration: BoxDecoration(
-                    //decorando a borda da Caixa
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    ),
-                  ),
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
+//manipulando os dado do Objeto Transaction
+          return Card(
+              child: Row(
+            children: [
+              //apresenta o valor da transação
+              Container(
+                //Adicionando margin ao container que irá apresentar
+                //o valor das transferências realizadas
+                margin: EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
+                ),
 
-                  //espaçamento entre os Elementos
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    "R\$ ${tr.value.toStringAsFixed(2)}",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, //negrito
-                        fontSize: 20,
-                        color: Colors.purple),
+                //decorando a Caixa que apresenta o preço do produto
+                decoration: BoxDecoration(
+                  //decorando a borda da Caixa
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
                   ),
                 ),
 
-                Column(
-                  //=========Titulo da Transferencia
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tr.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
+                //espaçamento entre os Elementos
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "R\$ ${tr.value.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, //negrito
+                      fontSize: 20,
+                      color: Colors.purple),
+                ),
+              ),
 
-                    //========Data da transferencia
-                    Text(
-                      DateFormat('d MMM y').format(tr.date),
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
+              Column(
+                //=========Titulo da Transferencia
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    tr.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
-                  ],
-                )
-              ],
-            ));
-          }).toList(), //converte para um List<Widget>
-        ),
+                  ),
+
+                  //========Data da transferencia
+                  Text(
+                    DateFormat('d MMM y').format(tr.date),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ));
+        },
       ),
     );
   }
--- Modulo_4/expenses/lib/components/transaction_user.dart
+++ Modulo_4/expenses/lib/components/transaction_user.dart
@@ -26,6 +26,91 @@ class _TransactionUserState extends State<TransactionUser> {
       value: 211.30,
       date: DateTime.now(),
     ),
+
+    //Conta #00
+    Transaction(
+      id: 't3',
+      title: 'Conta #00',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't4',
+      title: 'Conta #01',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't5',
+      title: 'Conta #02',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't5',
+      title: 'Conta #03',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't6',
+      title: 'Conta #04',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't7',
+      title: 'Conta #05',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't8',
+      title: 'Conta #06',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't9',
+      title: 'Conta #07',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't10',
+      title: 'Conta #08',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't11',
+      title: 'Conta #09',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't12',
+      title: 'Conta #10',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    //Conta #00
+    Transaction(
+      id: 't13',
+      title: 'Conta #11',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
   ];
 
   void _adiconarTransaction(String title, double value) {
