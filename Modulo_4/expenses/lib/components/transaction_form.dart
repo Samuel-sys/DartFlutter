44833a3c01fd9625f6dcdf0154ced0d32c6f06f3
Samuel-sys <alencar5647@gmail.com>
2020-11-10 13:01:58 -0300

    Criação do onSubmit na no TextField responsavel pelo cadastro do Titulo e do valor da transferencia Criação do metodo _submitForm que e responsavel por verificar se os dados forem preenchidos da forma correta e se estiverem corretos cadastra a Trasferencia na List se não ele não cadatra nem um novo Objeto na Lista

--- Modulo_4/expenses/lib/components/transaction_form.dart
+++ Modulo_4/expenses/lib/components/transaction_form.dart
@@ -3,9 +3,28 @@ import 'package:flutter/material.dart';
 class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+  // final dateController = TextEditingController();
+
   final void Function(String, double) onSubmit;
 
   TransactionForm({@required this.onSubmit});
+
+  _submitForm() {
+    //armazena o titulo da transação a variavel
+    final String title = this.titleController.text;
+
+    //tenta converter o valor digiatado para double caso não consiga
+    //atribui o valor 0
+    final value = double.tryParse(this.valueController.text) ?? 0.0;
+
+    //se um dos valores for invalido ele não efetua o cadastro
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    //essa function ira adicionar a nova transação a lista
+    onSubmit(title, value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +37,7 @@ class TransactionForm extends StatelessWidget {
           //campo de TITULO
           TextField(
             controller: titleController,
+            onSubmitted: (_) => _submitForm(),
             decoration: InputDecoration(
               labelText: 'Titulo',
             ),
@@ -25,26 +45,27 @@ class TransactionForm extends StatelessWidget {
 
           //campo de VALOR DE TRANSFERENCIA
           TextField(
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            onSubmitted: (_) => _submitForm(),
             controller: valueController,
             decoration: InputDecoration(
               labelText: 'Valor (R\$)',
             ),
           ),
 
+          // //campo da DATA DA TRANSFERENCIA
+          // TextField(
+          //   keyboardType: TextInputType.datetime,
+          //   controller: dateController,
+          //   decoration: InputDecoration(
+          //     labelText: 'Data',
+          //   ),
+          // ),
+
           //Botão de execução de cadastro
           Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
             FlatButton(
-              onPressed: () {
-                //armazena o titulo da transação a variavel
-                final String title = this.titleController.text;
-
-                //tenta converter o valor digiatado para double caso não consiga
-                //atribui o valor 0
-                final value = double.tryParse(this.valueController.text) ?? 0.0;
-
-                //essa function ira adicionar a nova transação a lista
-                onSubmit(title, value);
-              },
+              onPressed: _submitForm,
               child: Text(
                 "Nova Transação",
                 style: TextStyle(
