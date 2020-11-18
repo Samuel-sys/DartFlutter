44acf7b38f8b39d7c98cb7d537324dab4f3d1819
Samuel-sys <alencar5647@gmail.com>
2020-11-18 16:24:59 -0300

    Deletado o arquivo routes e transferido de volta para o metodo Main
    Criação de filtros de alimentos conforme a configuração que o usuario colocar no sistema

--- Modulo_7/meals/lib/main.dart
+++ Modulo_7/meals/lib/main.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:meals/utils/routes.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/settings.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/settings_screen.dart';
+import 'package:meals/screens/tabs_screen.dart';
+import 'package:meals/utils/app_routes.dart';
+
+import 'models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  void _filterMeal(Settings settings) {
+    //atualiza a lista de alimentos
+    setState(() {
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        //se a comida tiver gluten e o usuario selecionou a opção de comida
+        //sem gluten ele retorna false
+        final gluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final lactose = settings.isLactoseFree && !meal.isLactoseFree;
+
+        //se a comida for vegana e o usuario selecionar para aparecer apenas
+        //comidas veganas retorna true
+        final vegan = settings.isVegan && !meal.isVegan;
+        final vegetarian = settings.isVegetarian && !meal.isVegetarian;
+
+        return !gluten && !lactose && !vegan && !vegetarian;
+      }).toList();
+    });
+  }
+
   final themeApp = ThemeData(
     primarySwatch: Colors.pink,
     accentColor: Colors.amber,
@@ -20,11 +54,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Deli Meals',
+      title: 'Deli Meals', theme: themeApp, //tema do app
 
-      theme: themeApp, //tema do app
+      routes: {
+        AppRoutes.HOME: (_) => TabsScreen(), //pagina home
 
-      routes: routesPages, //arquivo com o Map com as rotas das Pages
+        AppRoutes.CATEGORIES_MEALS: (_) =>
+            CategoriesMealsScreen(this._availableMeals),
+
+        AppRoutes.MEAL_DETAILS: (_) => MealDetailsScreen(),
+
+        AppRoutes.SETTINGS: (_) => SettingsScreen(_filterMeal),
+      },
     );
   }
 }
