fcfbb0791bf33e67e2cec5579e06c6336b090ade
Samuel-sys <alencar5647@gmail.com>
2020-11-25 15:36:13 -0300

    Criação do método loadOrders para que os dados do App posa ser sincronizado com os do WebServer, atualizando a lista de pedidos feitos (_items).
    Alteração na page OrdersScreen, convertendo de um StatelessWidget para um StatefulWidget. Além de criação de uma ‘interface’ de inicialização onde que e feito a sincronização dos dados do App com o da API e colocado a função do evento onRefershIndicator para se fazer a atualização da lista de Pedidos feitos pelo usuário.

--- Modulo_10/shop/lib/providers/order.dart
+++ Modulo_10/shop/lib/providers/order.dart
@@ -22,7 +22,74 @@ class Orders with ChangeNotifier {
   final _baseUrl = 'https://flutter-shop-alencar.firebaseio.com/orders';
   List<Order> _items = [];
 
+  //entrega uma copia da lista de pedidos feito
   List<Order> get items => [...this._items];
+
+  //carrega os dados do cliente
+  Future<void> loadOrders() async {
+    //variável de controle onde os dados são manipulados
+    List<Order> loadedItems = [];
+
+    //recebe os dados do WebServer (API)
+    final response = await http.get("$_baseUrl.json");
+
+    //converte os dados entregues pela API (arquivo json) para um Map<String, dynamic>
+    final Map<String, dynamic> data = json.decode(response.body);
+
+    //se não tiver nenhum retorno da API ele não execulta esse bloco de comando
+    if (data != null) {
+      //loop de repetição para fazer cada item do Map com os dados dos pedidos
+      //se tornar um Objeto Order para ser adicionado no List loadedItems
+      data.forEach(
+        //(Chave, dado)
+        (orderId, orderData) {
+          //adiciona cada dado no loadedItems para depois se entregue ao this._items
+          loadedItems.add(
+            Order(
+              id: orderId,
+              //Id do Pedido
+
+              total: orderData['total'],
+              //valor total do pedido
+
+              date: DateTime.parse(orderData['date']),
+              //dia do pedido
+
+              //Lista de produtos comprados
+              products:
+                  //converte em uma List<dynamic>
+                  (orderData['products'] as List<dynamic>)
+                      .map(
+                        //converte o dados para o Objeto CartItem
+                        (items) => CartItem(
+                          //Id do item no carrinho
+                          id: items['id'],
+
+                          //Id do produto comprado
+                          productId: items['productId'],
+
+                          //nome do produto
+                          title: items['title'],
+
+                          //quantidade que foi comprada desse mesmo produto
+                          quantity: items['quantity'],
+
+                          //preço do produto
+                          price: items['price'],
+                        ),
+                      )
+                      .toList(), //converte para um List<CartItem>
+            ),
+          );
+        },
+      );
+    }
+    //informa os dados entregues da API para o List<Order> _items
+    this._items = loadedItems.reversed.toList();
+
+    //notifica ao App a alterção feita
+    notifyListeners();
+  }
 
   int get itemsCount => this._items.length;
 
--- Modulo_10/shop/lib/views/orders_screen.dart
+++ Modulo_10/shop/lib/views/orders_screen.dart
@@ -4,9 +4,30 @@ import 'package:shop/providers/order.dart';
 import 'package:shop/widgets/app_drawer.dart';
 import 'package:shop/widgets/order_widget.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  //responsável por informa a Page quando a List de Orders estive carregado
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    //carrega a lista de Objetos do tipo Orders que informa os pedidos efetuados
+    Provider.of<Orders>(context, listen: false)
+        .loadOrders() //método responsável por carregar os dados da API
+        .then((_) => setState(() {
+              this._isLoading = false; //avisa que os dados já foram carregados
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
+    //Provider de conexão com o WebServer (API)
     final Orders orders = Provider.of(context);
 
     return Scaffold(
@@ -17,12 +38,23 @@ class OrdersScreen extends StatelessWidget {
       ),
 
       //body
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) {
-          return OrderWidget(orders.items[i]);
-        },
-      ),
+      body:
+          //esta carregando a lista de produtos ainda ??
+          this._isLoading
+              ? Center(child: CircularProgressIndicator())
+              : RefreshIndicator(
+                  //caso ele ative o onRefreshe ele faz
+                  //uma atualização nos dados do sistema
+                  onRefresh: () => orders.loadOrders(),
+
+                  //lista de Pedidos feitos
+                  child: ListView.builder(
+                    itemCount: orders.itemsCount,
+                    itemBuilder: (ctx, i) {
+                      return OrderWidget(orders.items[i]);
+                    },
+                  ),
+                ),
     );
   }
 }
